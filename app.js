6170b85922620ec3650caf1542c9b0a3b6f96dfd
Garv Goel <goelgarv99@gmail.com>
2025-12-03 17:02:20 +0530

    parent and child

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-    const heading = React.createElement(
+const parent = React.createElement("div", {id: "parent"},
+    React.createElement("div", {id: "child"},React.createElement(
+        "h1",
+         {}, "i am an h1 tag" )
+ )
+);
+    
+    
+    
+    
+    
+    
+    
+    
+   /**  const heading = React.createElement(
         "h1",
          {id: "heading", xyz: "abc"}, 
          "Hello world");
-
+*/
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+
+    root.render(parent);
